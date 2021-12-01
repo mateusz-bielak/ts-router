bd368a18af4513024248e9fe8a121ebd83641e61
Mateusz Bielak <mt.bielak@gmail.com>
2021-12-01 16:59:52 +0100

    Map route elements to params

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
-const MOCKED_ROUTES = {
-  root: '/',
-  profile: '/profile/:id',
-} as const;
+type Key<K extends string> = { type: 'key'; value: K };
+type Parameter<P extends string> = { type: 'parameter'; value: P };
 
-type Static = { type: 'static'; value: string };
-type Parameter = { type: 'parameter'; value: string };
-type OptionalParameter = { type: 'optionalParameter'; value: string };
-
-type RouteParameter = Static | Parameter | OptionalParameter;
+type RouteParameter<T extends string> = Key<T> | Parameter<T>;
 
 type Route = keyof typeof MOCKED_ROUTES;
 
+const route = <T extends RouteParameter<string>[]>(...args: T) => args;
+const key = <K extends string>(value: K): Key<K> => ({ type: 'key', value });
+const param = <P extends string>(value: P): Parameter<P> => ({ type: 'parameter', value });
+
+const MOCKED_ROUTES = {
+  root: route(key('/')),
+  profile: route(key('profile'), param('id')),
+} as const;
+
 export const getPath = (route: Route) => MOCKED_ROUTES[route];
 
-export const to = (_route: Route, _params: Record<string, RouteParameter['value']>) => {
+export const to = <T extends Route>(_route: T, _params: typeof MOCKED_ROUTES[T]) => {
   return;
 };
+
+to('root', [{ type: 'key', value: '/' }]);
+to('profile', [
+  { type: 'key', value: 'profile' },
+  { type: 'parameter', value: 'id' },
+]);
